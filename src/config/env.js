1ba083a902e7701133929337affee12083a12998
Matheus Silveira <silveira.matheus.dev@gmail.com>
2026-08-14 17:45:50 -0300

    feat: add database connection

---
+++ src/config/env.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const requiredEnv = [
+    "DB_HOST",
+    "DB_USER",
+    "DB_PASSWORD",
+    "DB_NAME",
+];
+
+for (const variable of requiredEnv) {
+    if (!process.env[variable]) {
+        throw new Error(
+            `Variável de ambiente não configurada: ${variable}`
+        );
+    }
+}
+
+const env = {
+    port: process.env.PORT || 3306,
+
+    db: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    },
+}
